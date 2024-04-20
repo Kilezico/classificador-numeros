5b91bf8d35feca7b4048d3abcbf141812fa19ee7
KileZico <jgrangeirocosta@gmail.com>
2024-04-19 21:06:48 -0300

    apaga jpg quando sai

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from tkinter import messagebox
+import os
 
 from src.classifierModel import ClassifierModel
 from src.button import Button
@@ -76,6 +77,7 @@ def run():
         predict_but.draw(window)
         reset_but.draw(window)
 
+    os.remove("data/to_predict.jpg")
     pg.quit()
 
 
