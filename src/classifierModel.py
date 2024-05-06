4442c40c741bb6fe03ed458ef50c3889716423cd
KileZico <jgrangeirocosta@gmail.com>
2024-05-06 08:50:30 -0300

    Modelo treinado com mais Ums (1)

--- src/classifierModel.py
+++ src/classifierModel.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+from . import utility
+
 class ClassifierModel:
     def __init__(self, path_to_pickled_model):
         # Carrega o modelo pré-treinado
@@ -10,13 +12,16 @@ class ClassifierModel:
             self.model = pickle.load(f)
         
     def predict(self, file_to_jpeg):
-        # Processa a imagem para poder utilizá-la no modelo
+        # Lê a imagem
         image = cv2.imread(file_to_jpeg, cv2.IMREAD_GRAYSCALE)
         
-        image = cv2.resize(image, (16, 16))
-        _, image = cv2.threshold(image, 127, 1, cv2.THRESH_BINARY_INV)
-        
-        # Deixa ela unidimensional
+        # Processa a imagem para poder utilizá-la no modelo
+        image = utility.modify_images(image)
+
+        # # Mostra a imagem para propósitos de debugging
+        # plt.imshow(image, cmap='gray')
+        # plt.show()
+
         image = np.reshape(image, (1, -1))
 
         # Faz a classificação
